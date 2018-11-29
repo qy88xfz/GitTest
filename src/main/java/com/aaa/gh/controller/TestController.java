3c961c8f817bef98ba63202078510969814ba8a7
qy88xfz <767299286@qq.com>
2018-11-29 16:11:17 +0800

    Initial commit

---
+++ src/main/java/com/aaa/gh/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.aaa.gh.controller;
+
+/**
+ * className:TestController
+ * discription:
+ * author:xiefuzhi
+ * createTime:2018-11-29 16:08
+ */
+
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("我是傻逼");
+    }
+}
